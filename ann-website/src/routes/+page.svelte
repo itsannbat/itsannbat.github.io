6dcd2400bdb88a4ec90fc0573c468f270901f09f
abatur <abatur@uw.edu>
2023-06-29 12:12:48 -0700

    decided to do svelte

---
+++ ann-website/src/routes/+page.svelte
@@ -0,0 +1,90 @@
+<body>
+  <main>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand">Ann's Website</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page">Resume</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">Hobbies</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">Other Cool Stuff</a>
+              </li>
+            </ul>
+            <span class="navbar-text">
+              Add if needed
+            </span>
+          </div>
+        </div>
+      </nav>
+      <div style="background-color: white; width: 100%; height: 100%;">
+        <div style="background-color: white; width: 50%; height: 100%; float:left;">
+          <aside>
+            <div id="centered-aside">
+              <h1 id="name">Hello, my name is Ann.</h1>
+              <img src="ann.jpg" alt="Picture of Ann" id="profile">
+            </div>
+          </aside>
+        </div>
+
+        <div style="background-color: white; width:50%; height: 100%; float:left;">
+          <section>
+            <h1>Resume</h1>
+            <article>
+              <h2>Skills</h2>
+              <ul>
+                <li> Programming Languages and Frameworks: Java, JavaScript, CSS, HTML, SQL, Python, C, C++ </li>
+                <li> Tools: Git/GitHub, AWS, Visual Studio Code, Slack, Microsoft Office, API’s </li>
+              </ul>
+              <h2>Education</h2>
+              <ul>
+                <li>GPA: 3.6/4</li>
+                <li>Accepted into the UW Academy program, attending UW at the age of 16</li>
+                <li>UW Deans List for Fall 2020, Spring 2021, Fall 2021, Winter 2022, Spring 2022, Fall 2023, Winter 2023</li>
+                <li>Relevant Coursework: Data Structures and Parallelism, Intro to AI, Intro to
+                    Web Design, Intro to Database Systems, Calculus series, Foundations of
+                    Computing I & II, The Hardware/Software interface, Software Design and Implementation, Systems Programming
+                </li>
+              </ul>
+              <h2>Experience</h2>
+              <h4>Foursquare – Software Engineering Intern | June 2022 – September 2022</h4>
+              <ul>
+                <li>Worked with the data platform team to improve data dumps</li>
+                <li>Created an automated backup pipeline that saves 10-15 on call hours</li>
+                <li>Wrote from scratch a directed acyclic graph using Apache Airflow Operators that copies data from MongoDB to AWS S3 buckets that is used to optimize the data flow</li>
+                <li>Created a database table on AWS Athena that queries through CloudWatch logs to retrieve information about S3 objects</li>
+              </ul>
+              <h4>University of Washington Robinson Center – Teaching Assistant | June 2021 – January 2022</h4>
+              <ul>
+                <li>Reviewed and edited papers that 5-6th grade students wrote</li>
+                <li>Substituted for a teacher and taught a class on philosophy to 1st-8th grade students</li>
+                <li>Assisted the teacher with answering student’s questions and supervised the students during lunch periods, as well as pick up and drop off times</li>
+              </ul>
+              <h4>App Esteem – Deceptor Hunter | November 2018 – June 2019</h4>
+              <ul>
+                <li>Worked on helping app vendors develop and deliver consumer clean apps, by analyzing their current app and providing feedback</li>
+                <li>Created a project with Python that creates screenshots and organizes them by the select app that they are working with</li>
+              </ul>
+              <h2>Projects</h2>
+              <h4>Hiker Up | October 2022</h4>
+              <li>A full stack app that pairs hikers by their difficulty, whether they can carpool, which hikes they prefer, and by the number of hikers per hike. The app is written with React Native and Expo Go, and the backend uses Google Firebase.</li>
+              <h4>Teaching Assistant Helper | October 2021</h4>
+              <li>A machine learning bot written with Microsoft Azure hosted on a local host website that was written in JS that assists UW students by answering basic questions such as how many credits are required to graduate.</li>
+            </article>
+          </section>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384_geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </main>
+  </body>
+<footer>
+  <p>© Ann Baturytski. All rights reserved. Other attributions cited in document or
+  page source</p>
+</footer>
